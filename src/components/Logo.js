942d740b4c23443212504deaaeebc2b25a8fa150
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-08-21 22:31:31 +0300

    Added logo to nav bar and logic to nav buttons

---
+++ src/components/Logo.js
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+import React from 'react'
+import { motion } from 'framer-motion';
+
+const MotionLink = motion(Link);
+
+const Logo = () => {
+  return (
+    <div className='flex items-center justify-center mt-2'>
+      <MotionLink href="/"
+        className='w-16 h-16 bg-dark text-light flex items-center justify-center
+        rounded-full text-2xl font-bold
+        '
+        whileHover={{
+            backgroundColor: ["#121212", "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
+            scale: 1.25, // This will scale the element by 10% when hovered
+            transition: {
+                backgroundColor: { duration: 1, repeat: Infinity }, // Only apply the infinite loop to the backgroundColor
+                scale: { duration: 0.3 } // Separate transition for the scale effect
+            }
+        }}        
+      >IH</MotionLink>
+    </div>
+  )
+}
+
+export default Logo
