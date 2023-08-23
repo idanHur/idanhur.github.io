38a91d03b4f904e06f85c0e8821d93f546a5901e
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-08-24 01:01:18 +0300

    Addded light bulb to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import profilePic from "../../public/images/profile/home-img.JPG"
 import AnimatedText from '@/components/AnimatedText'
 import Link from 'next/link'
 import { LinkArrow } from '@/components/Icons'
+import lightBulb from '../../public/images/svgs/miscellaneous_icons_1.svg'
+
 
 export default function Home() {
   return (
@@ -44,6 +46,9 @@ export default function Home() {
             </div>
           </div>
         </Layout>
+        <div className='absolute right-8 bottom-8 inline-block w-24'>
+          <Image src={lightBulb} alt='IdanHur' className='w-full h-auto' />
+        </div>
       </main>
     </>
   )
