e9cca6c3e59cf115ca97b5edef008c6008496863
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-08-24 19:50:35 +0300

    Added small screen compatibility to about page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Layout = ({children, className=""}) => {
   return (
-    <div className={`w-full h-full inline-block z-0 p-32 ${className}`}>
+    <div className={`w-full h-full inline-block p-6 z-0 md:p-32 ${className}`}>
       {children}
     </div>
   )
