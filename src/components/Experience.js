b64b50a06cc777da8e2578316f8ad98430ed46ca
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-08-25 00:22:19 +0300

    Finished Experience animations

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,48 +1,68 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import {useScroll, motion} from "framer-motion"
+import LiIcon from './LiIcon'
 
 const Details =({position, company, companyLink, time, address, work}) => {
-    return <li className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col
-    items-center justify-between'>
-        <div>
-            <h3 className='capitalize font-bold text-2xl'>
-                {position}&nbsp;<a href={companyLink}
-                target='_blank'
-                className='text-primary capitalize'
-                >@{company}</a> </h3>
-            <span className='capitalize font-medium text-dark/75'>
-                {time} | {address}
-            </span>
-            <p className='font-medium w-full'>
-                {work}
-            </p>
-        </div>
-    </li>
-}
+        const ref = useRef(null);
+    return (
+        <li ref={ref} className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col
+        items-center justify-between'>
+            <LiIcon reference={ref}/>
+            <motion.div
+            initial={{y:50}}
+            whileInView={{y:0}}
+            transition={{duration:0.5, type:"spring"}}
+            >
+                <h3 className='capitalize font-bold text-2xl'>
+                    {position}&nbsp;<a href={companyLink}
+                    target='_blank'
+                    className='text-primary capitalize'
+                    >@{company}</a> </h3>
+                <span className='capitalize font-medium text-dark/75'>
+                    {time} | {address}
+                </span>
+                <p className='font-medium w-full'>
+                    {work}
+                </p>
+            </motion.div>
+        </li>
+    );
+};
 
 const Experience = () => {
+    const ref = useRef(null);
+    const{scrollYProgress} = useScroll(
+        {
+            target: ref,
+            offset: ["start end", "center start"]
+        }
+    )
   return (
     <div className='my-64'>
-      <h2 className='font-bold text-8xl mb-32 w-full text-center'>
+      <h2 className='font-bold text-7xl md:text-8xl mb-32 w-full text-center'>
         Experience
       </h2>
-      <div className='w-[75%] mx-auto relative'>
-        <ul className='w-full flex flex-col items-start justify-between ml-4'>
-            <Details 
-            position="Junior Android/Linux developer"
-            company="HTVet"
-            time="01/2023 - 05/2023"
-            companyLink="https://www.ht-vet.com/"
-            address="Hod HaSharon, Israel"
-            work="As the lead developer, I developed an Android app integrating UVC cameras and controlling microcontrollers. 
-            I adapted our software for Arch Linux tablets, involving debugging, code adjustments, and creating automation scripts. 
-            I enhanced system performance with automation scripts, managed various packages, and introduced Linux services. 
-            I implemented real-time WebRTC communication in Python for live camera feeds. I also set up a communication framework with microcontrollers via USB. 
-            On the management side, I organized tasks using Monday.com and managed code with GitLab, showcasing my ability to work independently and efficiently."
+      <div ref={ref} className='w-[75%] mx-auto relative'>
+            <motion.div 
+            style={{scaleY: scrollYProgress}}
+            className='absolute left-9 top-0 w-[4px] h-full bg-black origin-top'/>
+            <ul className='w-full flex flex-col items-start justify-between ml-4'>
+                <Details 
+                position="Junior Android/Linux developer"
+                company="HTVet"
+                time="01/2023 - 05/2023"
+                companyLink="https://www.ht-vet.com/"
+                address="Hod HaSharon, Israel"
+                work="As the lead developer, I developed an Android app integrating UVC cameras and controlling microcontrollers. 
+                I adapted our software for Arch Linux tablets, involving debugging, code adjustments, and creating automation scripts. 
+                I enhanced system performance with automation scripts, managed various packages, and introduced Linux services. 
+                I implemented real-time WebRTC communication in Python for live camera feeds. I also set up a communication framework with microcontrollers via USB. 
+                On the management side, I organized tasks using Monday.com and managed code with GitLab, showcasing my ability to work independently and efficiently."
+                />
 
-            />
-
-        </ul>
+            </ul>
       </div>
+      
     </div>
   )
 }
