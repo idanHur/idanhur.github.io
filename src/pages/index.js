87665a06b47313abe2bb989f7035c5ab587c28e2
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-08-26 19:32:37 +0300

    fixed bug

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,42 +21,42 @@ export default function Home() {
       
       <TransitionEffect />
 
-      <main className='flex items-center text-dark w-full min-h-screen
+      <main className="flex items-center text-dark w-full min-h-screen
       dark:text-light z-10
-      '>
-        <Layout className='pt-0'>
-          <div className='flex flex-col lg:flex-row items-center justify-center w-full'>
+      ">
+        <Layout className="pt-0">
+          <div className="flex flex-col lg:flex-row items-center justify-center w-full">
             
-            <div className='w-full px-4 lg:px-0 lg:w-1/2 flex flex-col items-center self-center'>
+            <div className="w-full px-4 lg:px-0 lg:w-1/2 flex flex-col items-center self-center">
               <AnimatedText text="Engineering Software Excellence Through Precision and Innovation." 
-              className='!text-3xl !text-center md:!text-5xl lg:!text-6xl'/>
-              <p className='my-4 text-base font-medium text-center '>
+              className="!text-3xl !text-center md:!text-5xl lg:!text-6xl"/>
+              <p className="my-4 text-base font-medium text-center ">
                 As a dedicated Software Engineer, I harness a rich palette of programming languages, 
                 frameworks, and tools to breathe life into visionary projects. 
                 Dive into my extensive portfolio, which mirrors my adeptness in C#, Python, Java, and more. 
                 Together, let's sculpt the future of software, one line of code at a time.
               </p>
-              <div className='flex items-center self-center mt-2'>
-                <Link href="/Idan Hur.pdf" target='_blank'
-                className='flex items-center bg-dark text-light p-2.5 px-6
+              <div className="flex items-center self-center mt-2">
+                <Link href="/Idan Hur.pdf" target="_blank"
+                className="flex items-center bg-dark text-light p-2.5 px-6
                 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
                 border-2 border-solid border-transparent hover:border-dark
                 dark:bg-light dark:text-dark hover:dark:bg-dark
                 hover:dark:text-light hover:dark:border-light
-                '
+                "
                 download={true}
                 >Resume <LinkArrow className={"w-6 ml-1"}/>
                 </Link>
-                <Link href="mailto:idan12060@gmail.com" target='_blank'
-                className='ml-4 text-lg font-medium capitalize text-dark 
-                underline dark:text-light'
+                <Link href="mailto:idan12060@gmail.com" target="_blank"
+                className="ml-4 text-lg font-medium capitalize text-dark 
+                underline dark:text-light"
                 >Contact</Link>
               </div>
             </div>
           </div>
         </Layout>
-        <div className='absolute right-8 bottom-8 lg:inline-block w-24 hidden'>
-          <Image src={lightBulb} alt='IdanHur' className='w-full h-auto' />
+        <div className="absolute right-8 bottom-8 lg:inline-block w-24 hidden">
+          <Image src={lightBulb} alt="IdanHur" className="w-full h-auto" />
         </div>
       </main>
     </>
