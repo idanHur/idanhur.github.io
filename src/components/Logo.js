9f833c6690406d549251f80f360572c1d7d8cec9
Idan Hur <62388861+idanHur@users.noreply.github.com>
2023-10-03 17:49:36 +0300

    Added reload page to logo if on home page

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -7,10 +7,10 @@ const MotionLink = motion(Link);
 const Logo = () => {
   return (
     <div className='flex items-center justify-center mt-2'>
-      <MotionLink href="/"
+      <MotionLink 
+        href="/"
         className='w-16 h-16 bg-dark text-light flex items-center justify-center
-        rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light
-        '
+        rounded-full text-2xl font-bold border border-solid border-transparent dark:border-light'
         whileHover={{
             backgroundColor: ["#121212", "rgba(131,58,180,1)", "rgba(253,29,29,1)", "rgba(252,176,69,1)", "rgba(131,58,180,1)", "#121212"],
             scale: 1.25, // This will scale the element by 10% when hovered
@@ -18,8 +18,16 @@ const Logo = () => {
                 backgroundColor: { duration: 1, repeat: Infinity }, // Only apply the infinite loop to the backgroundColor
                 scale: { duration: 0.3 } // Separate transition for the scale effect
             }
-        }}        
-      >IH</MotionLink>
+        }}
+        onClick={(e) => {
+          if (window.location.pathname === '/') {
+            e.preventDefault(); // Prevent the default link behavior
+            window.location.reload(); // Reload the page
+          }
+        }}
+      >
+        IH
+      </MotionLink>
     </div>
   )
 }
